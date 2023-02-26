32df83e70503a4c3cccb84ee289e84db47b2fbfb
NodeRunnerUA <118058348+Vladymirovich@users.noreply.github.com>
2023-02-26 15:43:17 +0100

    Update autodelegate_name.js

--- autodelegate_name.js
+++ autodelegate_name.js
@@ -1,27 +1,24 @@
 const shell = require("shelljs");
 
 // Enter your password
-const PASSWORD = 'wallet_password'
+const PASSWORD = '........'
 
 // Enter your valoper address
 const VALOPER = '.........'
 
 // Enter your wallet address
-const DELEGATOR = 'wallet_address'
+const DELEGATOR = '.........'
 
-// Enter your wallet2 address
-const DELEGATOR2 = 'wallet2_address'
-
-const CHAIN_ID = '..........'
+const CHAIN_ID = '.........'
 const WALLETNAME = 'wallet'
-const FEES = '8500'
-const DENOM = 'unibi'
-const PROJECT = 'nibid'
-var sleepTimeout = 300
+const FEES = '.........'
+const DENOM = 'ario'
+const PROJECT = '.........'
+var sleepTimeout = 60
 var next = false
 
 // Your start voting power
-var startStake = your_stake * 1000000
+var startStake = your current stake * 1000000
 
 function sleep(ms) {
     return new Promise((resolve) => {
@@ -29,8 +26,8 @@ function sleep(ms) {
     });
 }
 
-function getBalance(address) {
-    const cmdGetBalance = `${PROJECT} q bank balances ${address} -o json | jq -r '.balances | .[].amount'`
+function getBalance() {
+    const cmdGetBalance = `${PROJECT} q bank balances ${DELEGATOR} -o json | jq -r '.balances | .[].amount'`
     const result = shell.exec(cmdGetBalance, {shell: '/bin/bash', silent: true});
     const data = result.stdout + result.stderr;
     return data.split('\n')[0];
@@ -39,65 +36,55 @@ function getBalance(address) {
 function getTimeout(rewards, sleepTimeout) {
     const reward_per_sec = rewards / sleepTimeout;
     const procent = reward_per_sec / startStake;
-    const time = Math.sqrt((+FEES * 3 + 12500) / (reward_per_sec * procent));
+    const time = Math.sqrt((+FEES * 2) / (reward_per_sec * procent));
     console.log('time', sleepTimeout, 'rewards', rewards);
+    console.log('reward_per_sec:', reward_per_sec);
+    console.log('procent:', procent);
+    console.log('startStake', startStake);
+    //console.log(+FEES * 2);
     return time;
+    
 }
 
 (async function() {
     while (true) {
-        // Зібрати реварди
-        const startBalance = Number.parseInt(getBalance(DELEGATOR)) - +FEES * 100;
-        const cmdGetReward = `echo -e "${PASSWORD}\\ny\\n" | ${PROJECT} tx distribution withdraw-rewards ${VALOPER} --chain-id ${CHAIN_ID} --from ${WALLETNAME} --commission --fees ${FEES}${DENOM} -y`
+        const startBalance = Number.parseInt(getBalance()) - +FEES * 1000;
+        const cmdGetReward = `echo -e "${PASSWORD}\\ny\\n" | ${PROJECT} tx distribution withdraw-rewards ${VALOPER} --chain-id ${CHAIN_ID} --from ${WALLETNAME} --commission --fees ${FEES}${DENOM} --gas=300000 -y`
         console.log('Reward');
         console.log(cmdGetReward);
         
         const reward = shell.exec(cmdGetReward, {shell: '/bin/bash', silent: true});
         console.log(reward.stdout + reward.stderr);
-        console.log('Balance', getBalance(DELEGATOR));
+        console.log('Balance', getBalance());
 
-        // Зібрати з додаткового гаманця
-        await sleep(1000 * 6);
-        const startBalance2 = Number.parseInt(getBalance(DELEGATOR2)) - +FEES * 100;
-        const cmdGetAllReward = `echo -e "${PASSWORD}\\n${PASSWORD}\\n" | ${PROJECT} tx distribution withdraw-all-rewards --from ${DELEGATOR2} --chain-id ${CHAIN_ID} --fees 12500${DENOM} --gas=500000 -y`;
-        await sleep(1000 * 6);
+        /*await sleep(1000 * 6);
+        const cmdGetAllReward = `echo -e "${PASSWORD}\\n${PASSWORD}\\n" | ${PROJECT} tx distribution withdraw-all-rewards --from ${DELEGATOR} --chain-id ${CHAIN_ID} --fees 12500${DENOM} --gas=500000 -y`;
         console.log('All reward');
         console.log(cmdGetAllReward);
         const allReward = shell.exec(cmdGetAllReward, {shell: '/bin/bash', silent: true});
         console.log(allReward.stdout + allReward.stderr);
-        console.log('Balance2', getBalance(DELEGATOR2));
+        console.log('Balance', getBalance());*/
         
-        // Заделегувати з основного
-        await sleep(1000 * 6);
-        const balance = Number.parseInt(getBalance(DELEGATOR)) - +FEES * 100;
-        const cmdStakeAll = `echo -e "${PASSWORD}\\n${PASSWORD}\\n" | ${PROJECT} tx staking delegate ${VALOPER} ${balance}${DENOM} --from ${DELEGATOR} --chain-id ${CHAIN_ID} --fees ${FEES}${DENOM} -y`;
+        await sleep(1000 * 10);
+        const balance = Number.parseInt(getBalance()) - +FEES * 1000;
+        const cmdStakeAll = `echo -e "${PASSWORD}\\n${PASSWORD}\\n" | ${PROJECT} tx staking delegate ${VALOPER} ${balance}${DENOM} --from ${DELEGATOR} --chain-id ${CHAIN_ID} --fees ${FEES}${DENOM} --gas=300000 -y`;
         console.log('Stake all');
         const stakeAll = shell.exec(cmdStakeAll, {shell: '/bin/bash', silent: true});
         console.log(stakeAll.stdout + stakeAll.stderr);
         
-        //Заделегувати з додаткового
-        await sleep(1000 * 6);
-        const balance2 = Number.parseInt(getBalance(DELEGATOR2)) - +FEES * 100;
-        const cmdStakeAll2 = `echo -e "${PASSWORD}\\n${PASSWORD}\\n" | ${PROJECT} tx staking delegate ${VALOPER} ${balance2}${DENOM} --from ${DELEGATOR2} --chain-id ${CHAIN_ID} --fees ${FEES}${DENOM} -y`;
-        console.log('Stake all2');
-        const stakeAll2 = shell.exec(cmdStakeAll2, {shell: '/bin/bash', silent: true});
-        console.log(stakeAll2.stdout + stakeAll2.stderr);
-        console.log(cmdStakeAll2);
-        
-        
         if (next) {
-          const rewards = balance - startBalance + balance2 - startBalance2 - +FEES * 2;
+          const rewards = balance - startBalance + +FEES * 2;
           sleepTimeout = getTimeout(rewards, sleepTimeout);
-          startStake = startStake + rewards;   
+          startStake = startStake + rewards - +FEES * 2;
         }
         
         console.log('sleepTimeout', sleepTimeout)
         await sleep(1000 * sleepTimeout);
-        next = false
-        
-        if (sleepTimeout >= 60) {
-          next = true
+        next = true
+
+        if (+sleepTimeout < 10) {
+          sleepTimeout = 60
         }
-        
+
     }
 })();
